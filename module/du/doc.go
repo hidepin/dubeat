6adc9d255bba0faf8c9b205be7e775e7db4bb664
Hidetoshi Imai <hidepin@gmail.com>
2018-12-22 17:21:06 +0900

    Add generated dubeat files

---
+++ module/du/doc.go
@@ -0,0 +1,2 @@
+// Package du is a Metricbeat module that contains MetricSets.
+package du
---
+++ vendor/github.com/elastic/beats/script/generate_imports_helper.py
@@ -0,0 +1,1 @@
+/home/hidepin/.go/src/github.com/hidepin/dubeat/vendor/github.com/elastic/beats/metricbeat/scripts/generate_imports_helper.py
